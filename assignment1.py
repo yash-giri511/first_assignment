9e455544659bb20d49e8b8c393261f60512b9007
Yash Giri <yashgiri88550@gmail.com>
2025-08-14 13:10:22 +0530

    Add files via upload
    test

---
+++ assignment1.py
@@ -0,0 +1,12 @@
+#perform the basic mathematical operation on addition,subtration,multiplication and division
+
+#Input two numbers
+num1 = float(input("Enter first number : "))
+num2 = float(input("Enter second number : "))
+
+# Perform operation
+print("Addition =",num1+num2)
+print("Subtraction =",num1-num2)
+print("Multiplication =",num1*num2)
+print("Division =",num1/num2)
+
